ca35b62926aee01bbc3b5ec352563081ed84a0fd
byarustev <byarus45@gmail.com>
2019-11-08 11:51:08 +0300

    anagrams n chunk

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,53 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//soln 2
+function anagrams(stringA, stringB) {
+    return cleanString(stringA)===cleanString(stringB);
+
+}
+
+function cleanString(str){
+    return str
+            .replace(/[^\w]/g,'')
+            .toLowerCase()
+            .split('')
+            .sort()
+            .join('')
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//     //remove specials xters and put tolower case
+//     stringA.replace(/[^\w]/g,'').toLowerCase();
+//     stringB.replace(/[^\w]/g,'').toLowerCase();
+
+//     //create xter map
+//     stringMapA =buldCharMap(stringA);
+
+//     stringMapB = buldCharMap(stringB);
+
+//     if(stringA.length!==stringB.length){
+//         return false
+//     }
+
+//     for(char in stringMapB){
+//         if(stringMapB[char]!==stringMapA[char]){
+//             return false
+//         }
+//     }
+
+//     return true
+
+// }
+
+// function buldCharMap(str){
+//     //create xter map
+//     charMap ={}
+//     str.split('').sort().map(item=>{
+//         charMap[item]=charMap[item]+1 || 1
+//     });
+
+//     return charMap;
+// }
--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,53 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+/***
+ * algorithm (using slice)
+ * 1. create an empty chunked array
+ * 2. create start index at 0
+ * 3. while index < array.length
+ *    3.1 push a slice of length size from array into chuncked
+ *    3.2 add size to index
+ *
+ * NB. slice(startIndex, endIndex<--notinclusive)      
+ */
+function chunk(array, size) {
+    let chuncked=[]
+    let index = 0;
+    while(index<array.length){
+        let sliced=array.slice(index,size+index)
+        chuncked.push(sliced)
+
+        index+=size
+    }
+
+    return chuncked;
+}
+
+// chunk([1, 2, 3, 4], 2);
 
 module.exports = chunk;
+
+/***
+ * algorithm
+ * 1. create empty array to hold chucks called chunked
+ * 2. for each element in the unchunked array
+ *    2.1 retrieve the last element in the chunked
+ *    2.2 if the last element does not exist or its length is equal to chunk size
+ *        2.2.1 push a new chunk into chunked with the current element
+ *    2.3 else add the current element to the chunk 
+ * 
+ */
+// function chunk(array, size) {
+//     let chunked=[]
+//     array.map((item)=>{
+//         let lastItem = chunked[chunked.length-1]; //NB. its by reference so .push mutates the original source
+//         if(lastItem && lastItem.length!==size){
+//             lastItem.push(item)
+//         }else{
+//            chunked.push([item,]) 
+//         }
+//     })
+
+//     return chunked;
+// }
